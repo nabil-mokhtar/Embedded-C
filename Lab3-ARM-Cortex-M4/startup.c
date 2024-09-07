e6b04265256f8032be7dffc5fdaafe904d9205f1
Nabil <nabilmokhtar.t@gmail.com>
2024-09-07 20:39:24 +0300

    update lab3 startup

--- Lab3-ARM-Cortex-M4/startup.c
+++ Lab3-ARM-Cortex-M4/startup.c
@@ -1,9 +1,8 @@
-//Mastering_Embedded System online diploma 
 #include <stdint.h> 
-// #define STACK_Start_SP 0x20001000 
-// extern unsigned int stack_top; 
+
 extern int main(void);
 void Rest_Handler (void) ;
+
 static unsigned long stack_top[256];
 
 void Default_Handler()
@@ -14,40 +13,40 @@ void Default_Handler()
 void NMI_Handler(void) __attribute__((weak,alias("Default_Handler")));
 void H_fault_Handler(void) __attribute__((weak,alias("Default_Handler")));
 
-void (* const vector_functions[]) () __attribute__((section(".vectors")))=
+void (* const vector_functions[]) () __attribute__((section(".vectors"))) =
 {
-(void (*)()) ((unsigned long)stack_top+sizeof(stack_top)) ,
+(void (*)()) ((unsigned long)stack_top+sizeof(stack_top)),
 &Rest_Handler, 
-&NMI_Handler ,  
+&NMI_Handler,  
 &H_fault_Handler
 };
 
 
-extern unsigned int E_data;
-extern unsigned int S_data;
-extern unsigned int E_text;
-extern unsigned int E_bss;
-extern unsigned int S_bss;
+extern unsigned char E_data[];
+extern unsigned char S_data[];
+extern unsigned char E_text[];
+extern unsigned char E_bss[];
+extern unsigned char S_bss[];
 
 void Rest_Handler()
 {
 
-    unsigned int DATA_size=(unsigned char*) &E_data - (unsigned char*)&S_data ;
-    unsigned char* P_src = (unsigned char*) &E_text;
-    unsigned char* P_dest =(unsigned char*) &S_data;
+    unsigned int DATA_size= E_data - S_data ;
+    unsigned char* P_src =  E_text;
+    unsigned char* P_dest = S_data;
 
     for (int i=0 ; i<DATA_size;i++)
     {
-        *((unsigned char*)P_dest++)=*((unsigned char*)P_dest++);
+        *(P_dest++)=*(P_dest++);
     }
 
 
-    unsigned int BSS_size =(unsigned char*) &E_bss - (unsigned char*) &S_bss;
-    P_dest=(unsigned char*) &S_bss;
+    unsigned int BSS_size = E_bss - S_bss;
+    P_dest= S_bss;
 
     for (int i=0 ; i<BSS_size;i++)
     {
-        *((unsigned char*) P_dest ) = (unsigned char )0 ;
+        *( P_dest ) = (unsigned char ) 0 ;
     }
 
 
